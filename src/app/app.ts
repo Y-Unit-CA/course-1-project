e9f0eb6694f18cfcacec17d207a32c6d124db8cf
RY <codes@y-unit.com>
2025-07-11 06:02:20 -0700

    commit changed app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,37 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Data } from './services/data';
+import { Post } from './models/post';
 
 @Component({
   selector: 'app-root',
   imports: [RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 
 export class App {
   protected title = 'simplilearn-course-1-project-client-management-app';
-}
 
-// Service with HttpClient
-posts: Post[] = [];
+  posts: Post[] = [];
 
-onGetData(): void {
-  this.dataService.getPosts().subscribe({
-      next: (data) => {
-      console.log(data);
-      this.posts = data;
-    },
-    
-    error: (err) => {
-    console.error('Error: ${ JSON.stringify(err) }')
-    },
+  constructor(private router: Router, private dataService: Data) {
+    console.log('App component constructor executed!');
   }
-);
 
-console.log("onGetData() executed!");
-}
-
-// Dependency Injection
-constructor(private router: Router, private dataService: Data) {
-  console.log('App component constructor executed!');
+  onGetData(): void {
+    this.dataService.getPosts().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.posts = data;
+      },
+      error: (err) => {
+        console.error(`Error: ${ JSON.stringify(err) }`);
+      },
+    });
+    console.log("onGetData() executed!");
+  }
 }
--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
